7be2e0e702fc654112c1d98eb64afb8aefc7fecc
felixirwanto080 <felix_irwanto080@student.unud.ac.id>
2022-09-22 16:29:34 +0800

    Class dan operasi kalkulator sederhana

--- Program.cs
+++ Program.cs
@@ -1,64 +1,63 @@
 ﻿using System;  
-using System.Collections.Generic;  
-using System.Linq;  
-using System.Text;  
-using System.Threading.Tasks;  
+
 namespace Kalkulator_Felix {  
     class Program {  
         static void Main(string[] args) {  
-            Console.WriteLine("Enter the action to be performed");  
-            Console.WriteLine("Press 1 for Addition");  
-            Console.WriteLine("Press 2 for Subtraction");  
-            Console.WriteLine("Press 3 for Multiplication");  
-            Console.WriteLine("Press 4 for Division \n");  
-            int action = Convert.ToInt32(Console.ReadLine());  
+            Console.WriteLine("Pilih operasi kalkulator:");  
+            Console.WriteLine("Tekan \"1\": Penambahan");  
+            Console.WriteLine("Press \"2\": Pengurangan");  
+            Console.WriteLine("Press \"3\": Perkalian");  
+            Console.WriteLine("Press \"4\": Pembagian \n");  
+            int operasi = Convert.ToInt32(Console.ReadKey());
+            Console.Clear();
+
             Console.WriteLine("Enter 1st input");  
-            int input_1 = Convert.ToInt32(Console.ReadLine());  
+            double input_1 = Convert.ToDouble(Console.ReadLine());  
             Console.WriteLine("Enter 2nd input");  
-            int input_2 = Convert.ToInt32(Console.ReadLine());  
-            int result = 0;  
-            switch (action) {  
+            double input_2 = Convert.ToDouble(Console.ReadLine());  
+            double result = 0;  
+            switch (operasi) {  
                 case 1: {  
-                    result = Addition(input_1, input_2);  
+                    result = Penambahan(input_1, input_2);  
                     break;  
                 }  
                 case 2: {  
-                    result = Subtraction(input_1, input_2);  
+                    result = Pengurangan(input_1, input_2);  
                     break;
                     }  
                 case 3: {  
-                    result = Multiplication(input_1, input_2);  
+                    result = Perkalian(input_1, input_2);  
                     break;  
                 }  
                 case 4: {  
-                    result = Division(input_1, input_2);  
+                    result = Pembagian(input_1, input_2);  
                     break;  
                 }  
                 default:  
-                    Console.WriteLine("Wrong action!! try again");  
+                    Console.WriteLine("Operasi Kalkulator tidak ditemukan!");  
                     break;  
             }  
             Console.WriteLine("The result is {0}", result);  
             Console.ReadKey();  
         }  
-        //Addition  
-        public static int Addition(int input_1, int input_2) {  
-            int result = input_1 + input_2;  
+        // Class untuk melakukan operasi penambahan  
+        public static double Penambahan(double input_1, double input_2) {  
+            double result = input_1 + input_2;  
             return result;  
         }  
-        //Substraction  
-        public static int Subtraction(int input_1, int input_2) {  
-            int result = input_1 - input_2;  
+        // Class untuk melakukan operasi perkurangan  
+        public static double Pengurangan(double input_1, double input_2) {  
+            double result = input_1 - input_2;  
             return result;  
         }
-        //Multiplication  
-        public static int Multiplication(int input_1, int input_2) {  
-            int result = input_1 * input_2;  
+        // Class untuk melakukan operasi perkalian  
+        public static double Perkalian(double input_1, double input_2) {  
+            double result = input_1 * input_2;  
             return result;  
         }  
-        //Division  
-        public static int Division(int input_1, int input_2) {  
-            int result = input_1 / input_2;  
+        // Class untuk melakukan operasi pembagian  
+        public static double Pembagian(double input_1, double input_2) {  
+            double result = input_1 / input_2;  
             return result;  
         }  
     }  
